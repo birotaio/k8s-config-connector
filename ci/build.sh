148600f2bcc4eb1158eb7565ab16b7c40d749e2f
Julien Ammous <schmurfy@gmail.com>
2025-03-04 11:34:27 +0100

    bump version

--- ci/build.sh
+++ ci/build.sh
@@ -2,12 +2,16 @@
 VERSION=$(cat version/VERSION)
 ALLOWED_CRDS="ComputeAddress,ComputeForwardingRule,ComputeServiceAttachment,RunService,CloudSchedulerJob,IAMServiceAccount,IAMPolicyMember,IAMPartialPolicy,ComputeRegionNetworkEndpointGroup,ComputeBackendService,ComputeURLMap,ComputeTargetHTTPProxy,DNSRecordSet,PubSubTopic,EventarcTrigger"
 
-pushd "$(dirname "$0")" &> /dev/null
-
 # add nodeSelector to cnrm-system
-MANIFEST="../operator/channels/packages/configconnector/${VERSION}/cluster/gcp-identity/0-cnrm-system.yaml"
-ytt -f $MANIFEST -f patch-manifest.yaml > ../_manifest/templates/generated/generated.yaml
+mkdir -p _manifest/templates/generated
+MANIFEST="operator/channels/packages/configconnector/${VERSION}/cluster/gcp-identity/0-cnrm-system.yaml"
+ytt -f $MANIFEST -f ci/patch-manifest.yaml > ${MANIFEST}.tmp
+mv ${MANIFEST}.tmp $MANIFEST
 
-cd ../fix-manifests
+pushd fix-manifests &> /dev/null
 go mod tidy
 go run ./fix-operator.go ${ALLOWED_CRDS}
+popd &> /dev/null
+
+mv operator/Dockerfile .
+cp -r * ../build
