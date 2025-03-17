5ede22826e7d678b7ed8b0d7656dd019716bbb27
Julien Ammous <schmurfy@gmail.com>
2025-03-17 17:43:29 +0100

    fix last issues

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,5 @@
 
 VERSION=$(cat version/VERSION)
-ALLOWED_CRDS="ComputeAddress,ComputeForwardingRule,ComputeServiceAttachment,RunService,CloudSchedulerJob,IAMServiceAccount,IAMPolicyMember,IAMPartialPolicy,ComputeRegionNetworkEndpointGroup,ComputeBackendService,ComputeURLMap,ComputeTargetHTTPProxy,DNSRecordSet,PubSubTopic,EventarcTrigger"
 
 # add nodeSelector to cnrm-system
 mkdir -p _manifest/templates/generated
@@ -8,10 +7,42 @@ MANIFEST="operator/channels/packages/configconnector/${VERSION}/cluster/gcp-iden
 ytt -f $MANIFEST -f ci/patch-manifest.yaml > ${MANIFEST}.tmp
 mv ${MANIFEST}.tmp $MANIFEST
 
-pushd fix-manifests &> /dev/null
-go mod tidy
-go run ./fix-operator.go ${ALLOWED_CRDS}
-popd &> /dev/null
+
+mkdir -p _manifest/templates/crds
+mkdir -p _manifest/templates/operator
+rm -f _manifest/templates/crds/*.yaml
+
+
+CRDS=(
+  "computeforwardingrules.compute"
+  "computeaddresses.compute"
+  "computeforwardingrules.compute"
+  "computeserviceattachments.compute"
+  "runservices.run"
+  "cloudschedulerjobs.cloudscheduler"
+  "iamserviceaccounts.iam"
+  "iampolicymembers.iam"
+  "iampartialpolicies.iam"
+  "computeregionnetworkendpointgroups.compute"
+  "computebackendservices.compute"
+  "computeurlmaps.compute"
+  "computetargethttpproxies.compute"
+  "dnsrecordsets.dns"
+  "pubsubtopics.pubsub"
+  "eventarctriggers.eventarc"
+)
+
+for crd in "${CRDS[@]}"; do
+  cp config/crds/resources/apiextensions.k8s.io_v1_customresourcedefinition_${crd}.cnrm.cloud.google.com.yaml _manifest/templates/crds
+done
+
+cp operator/config/crd/bases/core.cnrm.cloud.google.com_configconnectors.yaml _manifest/templates/crds/
+cp operator/config/crd/bases/core.cnrm.cloud.google.com_configconnectorcontexts.yaml _manifest/templates/crds/
+
+
+make build-rbac-manifests
+cp operator/config/manager/manager.yaml _manifest/templates/operator/
+cp config/installbundle/release-manifests/rbac.yaml _manifest/templates/operator/
 
 mv operator/Dockerfile .
 cp -r * ../build
